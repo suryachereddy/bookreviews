bfff290771fb14fc19aed86b148834c892372750
suryachereddy <suryachereddy@gmail.com>
2020-05-06 18:22:35 +0530

    added import.py

---
+++ import.py
@@ -0,0 +1,24 @@
+import csv
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine("postgresql://postgres:bindu1973@localhost/project1")
+#edit remove os.getenv funtion in create_engine()
+db = scoped_session(sessionmaker(bind=engine))
+
+def main():
+    f = open("books.csv")
+    reader = csv.reader(f)
+    c=0
+    for isbn,title,author,year in reader:
+        db.execute("INSERT INTO books (isbn, title, author,year) VALUES (:isbn, :title, :author,:year)",
+                    {"isbn":isbn, "title":title, "author":author,"year":year})
+        #print(f"Added flight from {origin} to {destination} lasting {duration} minutes.")
+        c=c+1
+    print(c)
+    db.commit()
+
+if __name__ == "__main__":
+    main()
